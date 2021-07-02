5737b2b0f173c5ee273b59a0a93b577fe9c8fe35
WenHan <wenhantan94@gmail.com>
2021-07-02 15:23:01 +0800

    env file and api sorted, html mockup underway

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import APITest from "./components/APITest"
+import image from './assets/OWNit.png'
 
 function App() {
+
+  console.log(image)
   return (
     <div className="App">
-      <APITest />
+      {/* <APITest /> */}
+      <div className="topBar">
+        <img src={image} alt="logo" className="logo"/>
+      </div>
+      <nav>
+        <h1>Dashboard</h1>
+        <h1>Profile</h1>
+        <h1>Search</h1>
+      </nav>
+      <h1>Hello</h1>
     </div>
   );
 }
